ef9f1bb574374f9b8de6e939d724702872456a7d
KonQ1999 <michal.drago@gmail.com>
2021-10-21 15:31:45 +0200

    Initial commit

---
+++ src/main/java/pb/wi/kck/SerwerApplication.java
@@ -0,0 +1,13 @@
+package pb.wi.kck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pb/wi/kck/SerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package pb.wi.kck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
